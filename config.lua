2e96a0f2a6514bc989fa9266105c74fefc1e36c5
Cjneilson12 <96008403+Cjneilson12@users.noreply.github.com>
2022-04-01 23:47:14 +0100

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,6 @@
+Config = {}
+
+-- Call Admin Config --
+Config.calladmin_webhook = "https://discord.com/api/webhooks/959581066006511706/g78w-pGkBc2X31q20kR4HkWjvVu4sRSoVhbYc5rW2ZuH_WGANTZ7ro9ujx3qaz70WaLJ" -- Sends Call Admins To Discord
+Config.calladmin_title = "Admin Call:" -- Title For Call Admin Embed
+Config.calladmin_color = "3066993" -- GREEN = 65280 --- RED = 16711680
